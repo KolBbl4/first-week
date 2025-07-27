221582b3c8f2b4dee1ed80b8a6f3a4a5bebfe2ea
KolBbl4 <ko1ba@mail.ru>
2025-07-27 22:52:59 +0500

    Home works

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
-def main():
-    pass
+def reversString (words: str) -> None:
+    print(words[::-1] )
+
+def temperature(t:int, typeOfTemr: str)->int:
+    result = 0
+    if typeOfTemr == "F":
+        result = (t * 1.8) + 32
+    elif typeOfTemr == "C":
+        result = (t - 32) / 1.8
+    else:
+        raise "Не верно задан параметр"
+    return result
+
+def calculator(a: int, b: int, c:int)-> int:
+    if c == 0:
+        result= a + b
+    elif c == 1:
+        result = a* b
+    elif c == 2:
+        result = a // b
+    else:
+        raise "Не выбра параметр операции"
+
+    return result
